fdafc353641e2653fbf6f43254e8e8686a5e2a8b
dev <dev@airoglobal.com>
2022-10-27 12:16:28 +0530

    codes

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    public function store()
+    {
+        // return request()->all();
+
+        // create the user
+       $attributes = request()->validate([
+            'name' => ['required','max:255'],
+            'username' => ['required','min:3', 'max:255', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', 'max:255', Rule::unique('users', 'email')],
+            'password' => ['required','min:7','max:255'],
+        ]);
+
+        $user = User::create($attributes);
+
+        // log the user in
+        auth()->login($user);
+
+        // dd('success validation succeded');
+        return redirect('/movies')->with('success', 'Your account has been created');
+    }
+}
---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Validation\ValidationException;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+    public function store()
+    {
+        // Validate the request
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        // attempt to authenticate and log in the user
+        // based on the provided credentials
+        if (auth()->attempt($attributes)) {
+            session()->regenerate();
+
+            return redirect('/movies')->with('success', 'Welcome ');
+        }
+
+        // auth filed
+        throw ValidationException::withMessages([
+            'email' => 'Your provieded credentials could not be verified.'
+        ]);
+    }
+
+    public function destroy()
+    {
+        // ddd('log the use out');
+        auth()->logout();
+
+        return redirect('/')->with('success', 'you\'re out');
+    }
+}
